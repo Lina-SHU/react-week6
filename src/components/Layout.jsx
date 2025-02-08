e260e78cf437860b3315c67b33054678db5347b1
Lina-SHU <sakura0477@gmail.com>
2025-02-08 11:36:13 +0800

    feat: add gh-pages, isActive class

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,13 @@
 import { Outlet, NavLink } from "react-router";
 
 const Layout = () => {
+  const isActiveClass = (style) => {
+    return style.isActive ? 'header-nav-active' : ''
+  };
+
   return (<>
-    Layout
-    <NavLink to='/about'>about</NavLink>
+    <NavLink to='/about' className={isActiveClass}>about</NavLink>
+    <NavLink to='/products' className={isActiveClass}>products</NavLink>
     <Outlet />
   </>)
 };
