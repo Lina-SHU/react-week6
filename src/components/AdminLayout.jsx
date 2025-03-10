f7e2efcf56cc10e73aadb65eedde2d75225b4eea
Lina-SHU <sakura0477@gmail.com>
2025-03-10 17:42:46 +0800

    feat: update navbar

--- src/components/AdminLayout.jsx
+++ src/components/AdminLayout.jsx
@@ -3,9 +3,11 @@ import { Outlet, NavLink } from "react-router";
 const AdminLayout = () => {
   return (<>
     AdminLayout
+    <br />
     <NavLink to='/admin/products'>商品管理</NavLink> |
     <NavLink to='/admin/orders'>訂單管理</NavLink> |
     <NavLink to='/admin/coupons'>優惠券管理</NavLink>
+    <hr />
     <Outlet />
   </>)
 };
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -9,6 +9,7 @@ const Layout = () => {
     <NavLink to='/about' className={isActiveClass}>關於我們</NavLink> |
     <NavLink to='/products' className={isActiveClass}>商品列表</NavLink> |
     <NavLink to='/cart' className={isActiveClass}>購物車</NavLink>
+    <NavLink to='/login' className={isActiveClass}>登入</NavLink>
     <Outlet />
   </>)
 };
